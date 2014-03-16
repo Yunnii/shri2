e6b8902220496071edd9df9f85d0f8fb6da0a4c4
Luba Amiraslanova <yunnii@yandex-team.ru>
2014-03-17 00:32:52 +0600

    ie

--- js/index.js
+++ js/index.js
@@ -7,9 +7,15 @@
                .toggleClass('hover');
     }
 
-
     $(document).ready(function () {
+        $("td:nth-child(even)")
+            .toggleClass("even");
+
+        $("tr:nth-child(even)")
+            .toggleClass("even");
+
         $('td').hover(highlitingColumn);
         $('th').hover(highlitingColumn);
+
     });
 }());
